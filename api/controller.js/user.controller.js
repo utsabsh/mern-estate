baedc7fae95072e1b875778ed8e7027b56506451
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-12 11:55:24 +0545

    add delete user functionality

--- api/controller.js/user.controller.js
+++ api/controller.js/user.controller.js
@@ -11,7 +11,7 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(401, "you can only update your account!"));
   try {
     if (req.body.password) {
-      req.body.password = bcryptjs.hashSync(password, 10);
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
     const updateUser = await User.findByIdAndUpdate(
       req.params.id,
@@ -19,16 +19,26 @@ export const updateUser = async (req, res, next) => {
         $set: {
           username: req.body.username,
           email: req.body.email,
-          password: req.body.avater,
-          avatar: req.body.avater,
+          password: req.body.password,
+          avatar: req.body.avatar,
         },
       },
       { new: true }
     );
     const { password, ...rest } = updateUser._doc;
-    res.status(200).json({
-      rest,
-    });
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("user has been deleted");
   } catch (error) {
     next(error);
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useRef } from "react";
 import {
   getDownloadURL,
@@ -8,15 +8,27 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../../firebase";
+import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
+import { errorHandler } from "../../../api/utils/error";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePrec, setFilePrec] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
+  console.log(formData);
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -45,11 +57,55 @@ export default function Profile() {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl font-semibold text-center my-7">Profile</h1>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -59,7 +115,7 @@ export default function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          src={formData.avatar ? formData.avatar : currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
         />
@@ -78,29 +134,47 @@ export default function Profile() {
           type="text"
           placeholder="Username"
           className="rounded-lg p-3 border"
+          defaultValue={currentUser.username}
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           className="rounded-lg p-3 border"
+          defaultValue={currentUser.email}
           id="email"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           className="rounded-lg p-3 border"
+          // defaultValue={currentUser.password}
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "lodaing" : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer capitalize">
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer capitalize"
+        >
           Delete account
         </span>
         <span className="text-red-700 cursor-pointer capitalize">sign out</span>
+      </div>
+      <div className="text-center mt-3">
+        <span className="text-red-800">{error ? error : ""}</span>
+        <span className="text-green-800 capitalize">
+          {updateSuccess ? "successfully updated!" : ""}
+        </span>
       </div>
     </div>
   );
