5c2f193bf709e5d5984704d0974b87ea023c4227
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-12 09:47:41 +0545

    create upadte user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 mongoose
   .connect(process.env.mongo)
@@ -14,6 +16,7 @@ mongoose
   });
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
