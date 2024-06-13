bafa70831de67bd84318dccc090bb140dc57adb1
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-13 10:41:28 +0545

    complete user listing delete funcatinality

--- api/routes/lisiting.route.js
+++ api/routes/lisiting.route.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { createListing } from "../controller.js/lisiting.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controller.js/lisiting.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 export default router;
