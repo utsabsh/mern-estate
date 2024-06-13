25a0b9d07c8ad3731a1182292fef15efecba2722
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-13 19:09:16 +0545

    create search ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
-
+import Search from "./pages/Search";
 function App() {
   return (
     <BrowserRouter>
@@ -19,6 +19,7 @@ function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
