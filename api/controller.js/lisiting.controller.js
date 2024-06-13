3beef5fddd986d49edb21ec58cf362ff9a42f973
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-13 14:41:07 +0545

    complete update listing functionality

--- api/controller.js/lisiting.controller.js
+++ api/controller.js/lisiting.controller.js
@@ -28,21 +28,33 @@ export const deleteListing = async (req, res, next) => {
   }
 };
 
-export const updateListing = async () => {
+export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
-    return next(errorHandler(404, "Listing not Found"));
+    return next(errorHandler(404, "Listing not found!"));
   }
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(401, "You can only edit your own listings!"));
+    return next(errorHandler(401, "You can only update your own listings!"));
   }
+
   try {
     const updatedListing = await Listing.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true }
     );
-    res.status(200).json(updateListing);
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
--- api/routes/lisiting.route.js
+++ api/routes/lisiting.route.js
@@ -3,10 +3,12 @@ import {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } from "../controller.js/lisiting.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 export default router;
