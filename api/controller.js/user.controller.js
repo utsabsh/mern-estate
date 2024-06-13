3a96751f90a519f17cab9ec71d721ec5f984af36
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2024-06-13 09:22:31 +0545

    create get user listing api route

--- api/controller.js/user.controller.js
+++ api/controller.js/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({ messsage: "success 55" });
@@ -33,7 +34,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  if (req.user.id === req.params.id)
     return next(errorHandler(401, "You can only delete your account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
@@ -41,5 +42,17 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("user has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id != req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+    return next(errorHandler(401, "you can only view your own listing!"));
   }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListing,
 } from "./../controller.js/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -10,4 +11,5 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listing/:id", verifyToken, getUserListing);
 export default router;
